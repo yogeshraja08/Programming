f85e0bc4cd226bc6245832345e785459961dcc4d
YOGESH <yogeshraja45@gmail.com>
2023-07-21 12:19:26 +0530

    LUNCH UPDATE

--- Java Course/Java/Day5.java
+++ Java Course/Java/Day5.java
@@ -1,7 +1,36 @@
 
 public class Day5 {
     public static void main(String[] args) {
-        String a = "yon";
-        System.out.println(a.indexOf('y'));
+        // String a = "yogesh raja";
+        // System.out.println(a.indexOf('y'));
+        // System.out.print(a.substring(0,1).toUpperCase());
+        // System.out.print(a.substring(1,6).toLowerCase());
+        // System.out.print(a.substring(6,7));
+        // System.out.print(a.substring(7,8).toUpperCase());
+        // System.out.print(a.substring(8).toLowerCase());
+
+
+        // StringBuffer - can append values
+        // String - cannot append values
+        // StringBuffer txt = new StringBuffer("yon");
+        // String a = "yogesh";
+        // System.out.println(txt.capacity());
+        // System.out.println(txt.append(" raja"));
+        // // System.out.println(a.append(" raja"));
+        // System.out.println(txt.replace(2 , 3, "gesh"));
+        // System.out.println(txt.delete(0, 2));
+        // System.out.println(txt.insert(2, " "));
+
+
+        // int a=10;
+        // float b=20.22255f;
+        // String c = String.valueOf(a);
+        // String d = String.valueOf(b);
+        // System.out.println(c+d);
+
+
+        // String a = "hi guru".trim();
+        // Boolean b = a.startsWith("hi");
+        // System.out.println(b);
     }
 }
