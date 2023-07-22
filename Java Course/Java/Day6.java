588a1a9fba77b95c76f69bc21b1e8cda19384cab
YOGESH <yogeshraja45@gmail.com>
2023-07-22 13:33:09 +0530

    Create Day6.java

---
+++ Java Course/Java/Day6.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Day6 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number/word : ");
+        String number = input.next();
+        String rev = "", num = "";
+        for(int j=0; j<number.length()/2; j++){
+            num += number.charAt(j);
+        }
+        for(int i = number.length()-1; i>=number.length()/2; i--){
+            rev += number.charAt(i);
+        }
+        //System.out.println(num+rev);
+        try{
+            int k=Integer.parseInt(num+rev);
+            System.out.println(k);
+        }
+        catch(Exception e){
+            // System.out.println(e);
+            System.out.println(num+rev);
+        }
+        input.close();
+    }
+}
