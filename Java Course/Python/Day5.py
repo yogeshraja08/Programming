a9e728ef01fbcc43ee106011580256869515f8e1
YOGESH <yogeshraja45@gmail.com>
2023-07-25 20:23:49 +0530

    dinner update

--- Java Course/Python/Day5.py
+++ Java Course/Python/Day5.py
@@ -3,4 +3,41 @@
 # print(a[0].upper(),end="")
 # print(a[1:])
 
-word = input("Enter the word : ")
+#   # palindrome
+# word = input("Enter the word : ")
+# rev = word[::-1]
+# # arr = list(word)
+# # rev = ""
+# # for i in range (len(arr)-1,-1,-1):
+# #     rev += arr[i]
+# if (rev==word):
+#     print("It is palindrome")
+# else:
+#     print("It is not palindrome")
+
+# # find vowels
+# word = input("Enter the word : ")
+# arr = list(word)
+# vowels = ['a','e','i','o','u']
+# count = 0
+# for i in range(len(arr)):
+#     if (arr[i] in vowels):
+#         count+=1
+# print(count)
+
+# anagram
+word1 = input("Enter the word 1 : ")
+word2 = input("Enter the word 2 : ")
+arr1 = list(word1)
+arr2 = list(word2)
+count = 0
+if len(arr1) == len(arr2):
+    for i in range(len(arr1)):
+        for j in range(len(arr2)):
+            if (arr1[i]==arr2[j]):
+                count+=1
+                break
+if count == len(arr1):
+    print("It is anagram")
+else:
+    print("It is not anagram")
