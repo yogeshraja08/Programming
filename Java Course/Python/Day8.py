632239cfc29c3e0bc62346cd01a6c28ddddcffb5
YOGESH <yogeshraja45@gmail.com>
2023-07-31 16:16:56 +0530

    Update Day8.py

---
+++ Java Course/Python/Day8.py
@@ -0,0 +1,37 @@
+row1 = int(input("Enter the row 1 : "))
+col1 = int(input("Enter the col 1 : "))
+arr1 = []
+for i in range(row1):
+    subarr1 = []
+    for j in range(col1):
+        print("Enter the value for ",i,"X",j,end=": ")
+        subarr1.append(int(input()))
+    arr1.append(subarr1)
+
+row2 = int(input("Enter the row 2 : "))
+col2 = int(input("Enter the col 2 : "))
+arr2 = []
+for i in range(row2):
+    subarr2 = []
+    for j in range(col2):
+        print("Enter the value for ",i,"X",j,end=": ")
+        subarr2.append(int(input()))
+    arr2.append(subarr2)
+
+sum=[]
+if (row1==row2 and col1==col2):
+    for i in range(row1):
+        subsum = []
+        for j in range(col1):
+            subsum.append(arr1[i][j] + arr2[i][j])
+        sum.append(subsum)
+print("The sum of two matrix is ",sum)
+
+sub=[]
+if (row1==row2 and col1==col2):
+    for i in range(row1):
+        subsub = []
+        for j in range(col1):
+            subsub.append(arr1[i][j] - arr2[i][j])
+        sub.append(subsub)
+print("The sum of two matrix is ",sub)
