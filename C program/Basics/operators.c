2ef35a4d144e10ebbd8a00af90a0da32c79596fb
YOGESH <yogeshraja45@gmail.com>
2023-08-17 15:45:20 +0530

    update

---
+++ C program/Basics/operators.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+void main(){
+    printf(" Arithemetic Operator");
+    printf("+ , - , * , / , %%");
+    printf("\n");
+    printf(" Relational Operator");
+    printf("== , > , < , != , <= , >=");
+    printf("\n");
+    printf(" Logical Operator");
+    printf("&& , || , !");
+    printf("\n");
+    printf(" Assignment Operator");
+    printf("= , += , -= , *= , /= , %%=");
+    printf("\n");
+    printf(" Bitwise Operator");
+    printf("& , | , ^");
+    printf("\n");
+    printf(" Increment and Decrement Operator");
+    printf("\n");
+    printf(" Special Operator");
+}
---
+++ C program/test.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main(){
+    int x=11;
+    int y=7;
+    int a=8;
+    int b=1;
+    printf("%d",x|y);
+    printf("\n");
+    printf("%d",x&y);
+    printf("\n");
+    printf("%d",x^y);
+    printf("\n");
+    printf("%d",a<<b);
+    printf("\n");
+    printf("%d",a>>b);
+    printf("\n");
+    printf("%d",2^3);
+}
